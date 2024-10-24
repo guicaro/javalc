bb2161925b937739993ccb8175f30cd84d1bc861
Guillermo <guicaro@gmail.com>
2024-10-23 20:34:14 -0400

    Added solution and also updated pom for junit jupiter

--- src/main/java/com/problems/TwoSum.java
+++ src/main/java/com/problems/TwoSum.java
@@ -10,14 +10,27 @@ Example
 Input: nums = [2, 7, 11, 15], target = 9
 Output: [0, 1]
 Explanation: nums[0] + nums[1] == 2 + 7 == 9, so we return [0, 1]
+
  */
 
- /
+ import java.util.Map;
+ import java.util.HashMap;
 
 public class TwoSum {
   public int[] twoSum(int[] nums, int target) {
 	  // Solution goes here
+    Map<Integer, Integer> map = new HashMap<>();
 
-    return new int[] {1,2};
+    for (int i = 0; i < nums.length; i++) {
+        int complement = target - nums[i];
+
+	if (map.containsKey(complement)) {
+		return new int[] {map.get(complement), i};
+	}
+	
+	map.put(nums[i], i);
+    }
+
+    throw new IllegalArgumentException("There is no solution");
   }
 }
--- src/test/java/com/problems/TwoSumTest.java
+++ src/test/java/com/problems/TwoSumTest.java
@@ -2,16 +2,30 @@ package com.problems;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class TwoSumTest {
   
   @Test
-  public void shouldEqualArrays() {
+  public void shouldReturnAnswerWhenOneSolution() {
 	  TwoSum solution = new TwoSum();
 	  int[] nums = {2, 7, 11, 15};
 	  int target = 9;
 	  int[] result = solution.twoSum(nums, target);
-	  assertArrayEquals(new int[]{1,2}, result);
+	  assertArrayEquals(new int[]{0,1}, result);
+  }
+
+  @Test
+  public void shouldReturnException() {
+	TwoSum solution = new TwoSum();
+	int[] nums = {1, 3, 5};
+	int target = 99;
+	IllegalArgumentException e = assertThrows(
+		IllegalArgumentException.class,
+		() -> solution.twoSum(nums, target)
+	);
+
+	assertEquals("There is no solution", e.getMessage()); 
   }
 }
 
